f4ce47bd58c8a71f0c076f5a5835addaef7cd014
cavani12345 <edisonwacavan2015@gmail.com>
2021-02-06 15:11:15 -0800

    online digital shop

---
+++ shop/inc/footer.php
@@ -0,0 +1,71 @@
+2</div>
+   <div class="footer">
+   	  <div class="wrapper">	
+	     <div class="section group">
+				<div class="col_1_of_4 span_1_of_4">
+						<h4>Information</h4>
+						<ul>
+						<li><a href="about.php">About Us</a></li>
+						</ul>
+					</div>
+				<div class="col_1_of_4 span_1_of_4">
+					<h4>Why buy from us</h4>
+						<ul>
+						<li><a href="about.php">About Us</a></li>
+						<li><a href="index.php"><span>Search Terms</span></a></li>
+						</ul>
+				</div>
+				<div class="col_1_of_4 span_1_of_4">
+					<h4>My account</h4>
+						<ul>
+				   <li><a href="wishlist.php">My Wishlist</a></li>
+						<li><a href="faq.php">Help</a></li>
+						</ul>
+				</div>
+				<div class="col_1_of_4 span_1_of_4">
+					<h4>Contact</h4>
+						<ul>
+							<li><span>+250781299528</span></li>
+							<li><span>+250725705696</span></li>
+						</ul>
+				</div>
+			</div>
+			<div class="copy_right" style="background-color:white">
+				<p style="color:#F58220;font-size:20px;">Sup online shopping &amp; All rights Reseverd </p>
+		   </div>
+     </div>
+    </div>
+    <script type="text/javascript">
+		$(document).ready(function() {
+			/*
+			var defaults = {
+	  			containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+	 		};
+			*/
+			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	</script>
+    <a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
+    <link href="css/flexslider.css" rel='stylesheet' type='text/css' />
+	  <script defer src="js/jquery.flexslider.js"></script>
+	  <script type="text/javascript">
+		$(function(){
+		  SyntaxHighlighter.all();
+		});
+		$(window).load(function(){
+		  $('.flexslider').flexslider({
+			animation: "slide",
+			start: function(slider){
+			  $('body').removeClass('loading');
+			}
+		  });
+		});
+	  </script>
+
+</body>
+</html>
